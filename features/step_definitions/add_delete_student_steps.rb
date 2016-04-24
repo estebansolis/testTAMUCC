09f65d11d171f990a93cb3a59582013f21c6f262
cfajitas <cfajitas@gmail.com>
2016-04-24 19:29:04 +0000

    added step files

---
+++ features/step_definitions/add_delete_student_steps.rb
@@ -0,0 +1,27 @@
+Given(/^the following students exist:$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am on the manage page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I fill out the "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I press "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I expect to see  "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I select \#s$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I expect to see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/step_definitions/track_late_steps.rb
@@ -0,0 +1,32 @@
+Given(/^following suits are rented:$/) do |table|
+  # table is a Cucumber::Core::Ast::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am on the home page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I follow the "([^"]*)" link$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I expect to be on the Checked Out page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I expect to see suit "([^"]*)" on the list of checked out suits$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I expect to be on the Late page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I expect to see suit "([^"]*)" on the list of late suits$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I expect not to see suits "([^"]*)", "([^"]*)" , and "([^"]*)" checked out$/) do |arg1, arg2, arg3|
+  pending # Write code here that turns the phrase above into concrete actions
+end
