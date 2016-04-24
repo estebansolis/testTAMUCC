1a91b8d37d425bb6bee05fab3e0f952c09072ea5
cfajitas <cfajitas@gmail.com>
2016-04-24 20:34:12 +0000

    Rspec added

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,8 @@
 class Rental < ActiveRecord::Base
+    validates :Rental_ID, presence: true
+    validates :UIN, presence: true
+    validates :Apparel_ID, presence: true
+    validates :Checkout_Date, presence: true
+    validates :Expected_Return_Date, presence: true
+    validates :Return_Date, presence: true
 end
