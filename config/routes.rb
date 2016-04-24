3518596f155416997ea6e9e33e638429766d8864
NHB <bain607@gmail.com>
2016-04-24 15:40:39 -0500

    Initial version of Checkout page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get 'students' => "students#index"
   get 'checkedout'=> "pages#checkedout", as: :checkedout
   get 'checkin'=> "pages#checkin", as: :checkin
-  get 'checkout'=> "pages#checkout", as: :checkout
+  get 'checkout'=> "rentals#new", as: :checkout
   get 'late'=> "pages#late", as: :late
   get 'manage'=> "pages#manage", as: :manage
   get 'signin'=> "pages#signin", as: :signin
