a507e0d60c3b9c4883ef6a3526e356a2248e8007
cfajitas <cfajitas@gmail.com>
2016-04-26 23:54:45 +0000

    Installed simplecov and Cucumber Progress

--- features/step_definitions/add_delete_student_steps.rb
+++ features/step_definitions/add_delete_student_steps.rb
@@ -4,22 +4,27 @@ Given(/^the following students exist:$/) do |table|
   end
 end
 
+Given(/^A user is logged in as "([^"]*)"$/) do |arg1|
+  @current_user = User.create!(
+    :password => 'generic',
+    :password_confirmation => 'generic',
+    :email => "#{arg1}@example.com"
+  )
+  visit "/signin" 
+  fill_in("Email", :with => "#{arg1}@example.com") 
+  fill_in("Password", :with => 'generic') 
+  click_button("Log in")
+  if page.respond_to? :should
+    page.should have_content('Signed in successfully')
+  else
+    assert page.has_content?('Signed in successfully')
+  end
+end
+
 When(/^I fill in the "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
-  fill_in(arg1,arg2)
+  fill_in(arg1, :with => arg2)
 end
 
-Then(/^I expect to see  "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^I see the UIN (\d+)$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^I expect to see "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When(/^I click "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I confirm popup$/) do
+  page.driver.browser.switch_to.alert.accept 
 end
