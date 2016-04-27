7f677b5c56bee497a80bc7716b96765432cb11b1
NHB <bain607@gmail.com>
2016-04-26 20:45:21 -0500

    Fixed routing after creating new entries in models

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -29,7 +29,7 @@ class RentalsController < ApplicationController
 
     respond_to do |format|
       if @rental.save
-        format.html { redirect_to @rental, notice: 'Rental was successfully created.' }
+        format.html { redirect_to rentals_path}
         format.json { render :show, status: :created, location: @rental }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class RentalsController < ApplicationController
   def update
     respond_to do |format|
       if @rental.update(rental_params)
-        format.html { redirect_to @rental, notice: 'Rental was successfully updated.' }
+        format.html { redirect_to rentals_path}
         format.json { render :show, status: :ok, location: @rental }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class RentalsController < ApplicationController
   def destroy
     @rental.destroy
     respond_to do |format|
-      format.html { redirect_to rentals_url, notice: 'Rental was successfully destroyed.' }
+      format.html { redirect_to rentals_url}
       format.json { head :no_content }
     end
   end
